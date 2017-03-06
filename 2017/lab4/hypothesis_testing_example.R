dc88b32300475f7dfa33adce3e0caf057203e8d3
Lee Zamparo <zamparo@gmail.com>
2017-03-06 17:05:08 -0500

    added (cleaned up and annotated) version of our notes for lecture today

--- 2017/lab4/hypothesis_testing_example.R
+++ 2017/lab4/hypothesis_testing_example.R
@@ -4,9 +4,9 @@ library(ggplot2)
 install.packages("downloader")
 library(downloader)
 
-# Get some data to play w.
+# Get some data to play with: weights of mice fed regular vs high-fat diet.
 short_url <- "http://bit.ly/2mx6KIR"
-#url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
+# url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
 filename <- "femaleMiceWeights.csv" 
 download(short_url, destfile=filename)
 dat <- read.csv("femaleMiceWeights.csv")
@@ -16,34 +16,36 @@ control <- filter(dat,Diet=="chow") %>% select(Bodyweight) %>% unlist
 treatment <- filter(dat,Diet=="hf") %>% select(Bodyweight) %>% unlist
 print( mean(treatment) )
 
-# calculate the difference in the mean(control), mean(treatment)
+
+# We'd like to determine: is there a statistically significant effect of diet on the weights of these mice?
+# We can't change the design of the experiment, but we can recognize the elements involved and validate  
+# whether or not the conclusion is meaningful or not.
+#
+# First let's pick a leve of significance (alpha): 0.05
+
+# Next, let's decide on a test statistic: the difference between the mean(treatment) and mean(control)
 obsdiff <- mean(treatment) - mean(control)
 
+# The control group in the above comparison was sampled (uniformly at random without replacement, AFAIK) from a 
+# larger control population, provided below here.  Let's get it, and us it to construct an empirical null distribution of
+# our test statistic
 
-# # Control pop'n
 shorter_URL <- "http://bit.ly/2mu5Z2Z"
 # url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
 filename <- "femaleControlsPopulation.csv"
-if (!file.exists(filename)) download(url,destfile=filename)
-population <- read.csv(filename)
-population <- unlist(population) # turn it into a numeric
-View(population)
+if (!file.exists(filename)){
+  download(shorter_URL,destfile=filename)
+  population <- read.csv(filename)
+} 
 
-# take samples from control pop'n
-control <- sample(population$Bodyweight[13:24],8)
+# we take samples from control pop'n like so:
+control <- sample(population$Bodyweight,12)
 
-# establish an empirical null distribution of the
-# mean of the weights of control mice
-n <- 10000
-null <- vector("numeric",n)
-for (i in 1:n) {
-  control <- sample(population$Bodyweight[1:12],8)
-  treatment <- sample(population$Bodyweight[13:24],8)
-  null[i] <- mean(treatment) - mean(control)
-}
+# Now we're ready to establish an empirical estimate of our distribution of our test statistic.
+# Under the null distribution: there is no difference between the average weights in the high-fat diet mice and regular diet mice groups.
+# So, if we randomly sample 24 mice from the control group, splitting them between 'treatment' and 'control', and take the mean(treatment) - mean(control),
+# we can get an estimate of the distribution of our test statistic under the null hypothesis.
 
-
-# establish an empirical null distribution of the mean of the weights of control mice
 n <- 10000
 null <- vector("numeric",n)
 for (i in 1:n) {
@@ -52,15 +54,49 @@ for (i in 1:n) {
   null[i] <- mean(treatment) - mean(control)
 }
 
-# what is the mean difference in treatment vs control from our simulation?
+# put our observations into a data.frame
+null_df <- as.data.frame(null)
+
+# Now we can compute: what is the likelihood of observing a difference between the treatment and control groups 
+# which is as large or larger than obsdiff?
 mean(null >= obsdiff)
 
 
-# plot the distribution, with our rejection region
+# let's plot the empirical null distribution, with a rejection region corresponding to alpha = 0.05
 ggplot(null_df, aes(x = null)) + 
   geom_histogram() + 
   geom_vline(xintercept=1.96, linetype='dotted', color="red") + 
   geom_vline(xintercept=-1.96, linetype='dotted', colour="red") +
   geom_vline(xintercept = obsdiff) + 
+  annotate("text",x=-obsdiff + 0.05, y=800,label="rejection region corresponding to alpha = 0.05",size=3, color="red") + 
+  annotate("text",x=obsdiff + 1.05, y=800,label="<- observed difference",size=3, color="black") + 
   ggtitle("Empirical distribution of the differences between mean(treatment) - mean(control)")
 
+# So, it looks as if we have good reason to reject the null hypothesis at this significance level alpha, at least if we model 
+# the null distribution as a standard normal.  Is it a good fit?
+qqnorm(null_df$null)
+qqline(null_df$null)
+
+# yeah, it's a good fit :)
+
+# This test we did, where we construct an empirical null by repeated random sampling, is only one possible way to perform the test.  
+# Another common way is to model the null distribution as a student-T, and do what's called a T-test.
+# Read about that test here: http://genomicsclass.github.io/book/pages/t-tests_in_practice.html
+
+
+
+###
+# For posterity: here's the differences between the mean of the treatment group and control group 
+# which I mistakenly calcuated.
+# N.B: this is just to establish that the estimate of each group mean is precise, 
+# it will not build the empirical null distribution of the test statistic
+
+n <- 10000  # number of simulations to run
+null <- vector("numeric",n)
+for (i in 1:n) {
+  control <- sample(population$Bodyweight,12) # sample 12 control mice
+  null[i] <- mean(treatment) - mean(control)  # take the difference between the treatment mice and those control mice
+}
+
+# we should see a value of ~ 0.5 for mean(null >= obsdata)
+mean(null >= obsdiff)
